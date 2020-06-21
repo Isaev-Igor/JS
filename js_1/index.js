b9105b28fb13504e850414c1028fd10df79846e1
Isaev Igor <igor.igorovich.isaev@gmail.com>
2020-06-21 14:27:48 +0300

    1

---
+++ js_1/index.js
@@ -0,0 +1,20 @@
+var number = 13;
+console.log(typeof number);
+
+var words = "lake";
+console.log(typeof words);
+
+var question = false;
+console.log(typeof question);
+
+var password;
+console.log(password);
+
+var age = null;
+console.log(typeof age);
+
+var name = prompt("Введите Ваше имя", "Здесь должно быть Ваше имя");
+alert("Ученик " + name + " знает JavaSctipt" );
+
+var task = confirm("Tы точно знаешь JavaSctipt?");
+console.log(task);
---
+++ js_2/index.js
@@ -0,0 +1,54 @@
+// var answer = prompt("Какое официальное название JavaScript", "");
+// if (answer != "ECMAscript") {
+//     alert("Не знаете? ECMAscript");
+// } else {
+//     alert("Верно!");
+// }
+
+
+// var answer = prompt("Укажи любое число", "");
+// if (answer > 0) {
+//     alert("1");
+// } else if (answer < 0) {
+//     alert("-1");
+// } else {
+//     alert("0");
+// }
+
+
+
+
+// var answer = prompt("Кто пришел?", "");
+// if (answer === "Админ") {
+//     var pass = prompt("укажите пароль");
+//     if (pass === "Чёрный властелин") {
+//         alert("Добро пожаловать!");
+//     } else if (pass == null) {
+//         alert("Вход отменен");
+//     } else {
+//         alert("пароль неверен");
+//     }
+// } else if (answer == null) {
+//     alert("Вход отменен");
+// } else {
+//     alert("я вас не знаю");
+// }
+
+
+// var i = 0;
+// while (i < 3) {
+//     alert("номер " + i + "!");
+//     i++;
+// }
+
+
+
+
+var answer = prompt("Укажи число больше 100", "");
+if (answer > 0) {
+    alert("1");
+} else if (answer < 0) {
+    alert("-1");
+} else {
+    alert("0");
+}
