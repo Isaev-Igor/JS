dc7ab7e4bb9b75658569efa5a5148f4faf32d7bb
Isaev Igor <igor.igorovich.isaev@gmail.com>
2020-06-30 00:22:13 +0300

    1

--- js_3/main.js
+++ js_3/main.js
@@ -1,20 +1,33 @@
+// function random(number) {
+//     return Math.floor(Math.random() * (number + 1));
+// }
+//
+// var test = document.querySelectorAll('.test');
+// window.addEventListener('click', changeColor);
+// function clickNumber() {
+//
+// }
+// function changeColor() {
+//     for (var i = 0; i < test.length; i++) {
+//
+//         var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+//         test[i].style.backgroundColor = rndCol;
+//     }
+// }
+
+
 function random(number) {
     return Math.floor(Math.random() * (number + 1));
 }
 
+
 var test = document.querySelectorAll('.test');
-window.addEventListener('click', changeColor);
-function clickNumber() {
-
+for (var i = 0; i < test.length; i++) {
+    test[i].addEventListener('click', changeColor);
 }
+
 function changeColor() {
-    for (var i = 0; i < test.length; i++) {
-
-        var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-        test[i].style.backgroundColor = rndCol;
-    }
+    var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    this.style.backgroundColor = rndCol;
 }
-
-
-
 
